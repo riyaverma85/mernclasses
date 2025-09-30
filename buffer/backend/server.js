271abcac431e7a34c0fcce6644aa55b1dda44310
riyaverma85 <riyaverma2976@gmail.com>
2025-09-29 18:29:07 -0700

    kghg

--- buffer/backend/server.js
+++ buffer/backend/server.js
@@ -1,24 +1,28 @@
 
-//=======================================FORK LENGHT CODE==================================================================
+//=======================================FORK LENGTH CODE==================================================================
 const http=require("http");
 const os=require("os");
 const cluster=require("cluster");
-
-console.log(os.cpus().length);
-
 if(cluster.isMaster){
-    console.log(os.cpus().length);
+   
     const frkLength=os.cpus().length;
+     console.log(`Master process PID:${process.pid}`);
+     console.log(`Forking ${frkLength} workers......`)
     for(var i=0; i<frkLength; i++){
         cluster.fork();
     }
-    
+
+
+//
+cluster.on("exit",(worker,code,signal)=>{
+    console.log(`Worker ${worker.process.pid} died.Forking a new worker....`);
+    cluster.fork();
+});
 }
 else{
     http.createServer((req,res)=>{
-    setTimeout(()=>{
-        res.write("<h1>welcome to node js App </h1>")
-        res.end()
-    },2000)
+      res.write("<h1>welcome to node js App </h1>")
+      res.end(`Handled by worker process PID:${process.pid}\n`);
+ 
 }).listen(8000)
 }
