ee935a09f00fe1054fa00621852acd1086143540
riyaverma85 <riyaverma2976@gmail.com>
2025-10-16 14:56:52 -0700

    user

--- cluster/backend/models/User.js
+++ cluster/backend/models/User.js
@@ -1,12 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   age: Number
-// });
-
-// module.exports = mongoose.model("User", userSchema);
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
